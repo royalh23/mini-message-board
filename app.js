9b505e98afe0d02d488fffef32be55643b50cc30
Royal <royalhuseyn902@gmail.com>
2024-08-02 02:23:11 +0400

    Create index.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const path = require('node:path');
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/indexRouter');
 
 const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.use('/', indexRouter);
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,21 +1,7 @@
 const { Router } = require('express');
+const indexController = require('../controllers/indexController');
 const router = Router();
 
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date(),
-  },
-];
-
-router.get('/', (req, res) => {
-  res.send('Hello');
-});
+router.get('/', indexController.getIndex);
 
 module.exports = router;
