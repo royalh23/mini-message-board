54f73c661efd8cf466ef2b57ddcfe0f2567a2f5e
Royal <royalhuseyn902@gmail.com>
2024-08-02 02:29:14 +0400

    Refactor to remove controllers

--- controllers/indexController.js
+++
@@ -1,7 +0,0 @@
-const messages = require('../utils/messages');
-
-const getIndex = (req, res) => {
-  res.render('index', { title: 'Mini Message Board', messages: messages });
-};
-
-module.exports = { getIndex };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,20 @@
 const { Router } = require('express');
-const indexController = require('../controllers/indexController');
 const router = Router();
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
 
-router.get('/', indexController.getIndex);
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
 
 module.exports = router;
