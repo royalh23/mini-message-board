16d3b88e76890662ee347c5da7af6470e75c2b97
Royal <royalhuseyn902@gmail.com>
2024-08-02 01:52:10 +0400

    Create messages array in indexRouter.js

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 router.get('/', (req, res) => {
   res.send('Hello');
 });
